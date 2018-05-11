762622e7c4b93ad2a82e9a35222ade6b035d0bf2
chengzhiyong <chengzhiyong@hztianque.com>
2018-05-11 10:30:11 +0800

    cy-carlton

---
+++ cy-carlton/cy-carlton-eureka/src/main/java/com/carlton/cycarltoneureka/CyCarltonEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.carlton.cycarltoneureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyCarltonEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyCarltonEurekaApplication.class, args);
+	}
+}
