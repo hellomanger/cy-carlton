19c86bb506d78035561c9f14d444f038544ec802
chengzhiyong <chengzhiyong@hztianque.com>
2018-05-11 11:30:57 +0800

    commit

--- cy-carlton/cy-carlton-eureka/src/main/java/com/carlton/cycarltoneureka/CyCarltonEurekaApplication.java
+++ cy-carlton/cy-carlton-eureka/src/main/java/com/carlton/cycarltoneureka/CyCarltonEurekaApplication.java
@@ -2,8 +2,10 @@ package com.carlton.cycarltoneureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class CyCarltonEurekaApplication {
 
 	public static void main(String[] args) {
